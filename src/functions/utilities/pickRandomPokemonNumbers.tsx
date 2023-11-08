83d17224ae387b59373e03b2da3478171ed0532e
CarlG <carl91y@gmail.com>
2023-11-08 10:31:17 -0800

    fixed bug where random pokemon generating function would sometimes have duplicates

--- src/functions/utilities/pickRandomPokemonNumbers.tsx
+++ src/functions/utilities/pickRandomPokemonNumbers.tsx
@@ -1,7 +1,13 @@
 export const pickRandomPokemonNumbers = (): number[] => {
     const numberArray: number[] = [];
     for (let i = 0; i < 6; i++) {
-        numberArray.push(Math.floor(Math.random() * (255 - 1) + 1));
+        let num: number;
+        do {
+            num = Math.floor(Math.random() * (255 - 1) + 1);
+        } while (numberArray.includes(num));
+        numberArray.push(num);
+        console.log("pushed" + num);
     }
+    console.log(numberArray);
     return numberArray;
 };
