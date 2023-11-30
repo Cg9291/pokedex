12d29c253a5332ffd5bf1b437356c450462abd4f
nomad <pamirkantar21@gmail.com>
2023-11-29 20:47:20 -0800

    PMR-3: game logic completed with basic styling

--- src/functions/api/singleApiCalls/getPokemonGameList.tsx
+++ src/functions/api/singleApiCalls/getPokemonGameList.tsx
@@ -6,12 +6,14 @@ import { endpoints } from "../../../objects/endpoints";
 export type PokemonGuessInfo = {
     pokemonName: string;
     pokemonSprite: string;
+    colour: string;
 };
 
 type PokemonListResponse = {
     name: string;
     url: string;
 };
+const FIRST_GEN_COUNT = 151;
 
 export function getPokemonGameList() {
     const [pokemons, setPokemons] = useState<PokemonGuessInfo[]>([]);
@@ -19,22 +21,22 @@ export function getPokemonGameList() {
 
     useEffect(() => {
         (async () => {
-            await fetchPokemons(151);
+            await fetchPokemons(FIRST_GEN_COUNT);
         })();
     }, []);
 
-    async function fetchPokemons(limit: number) {
+    async function fetchPokemons(limit: number = FIRST_GEN_COUNT) {
         try {
             setPending(true);
             const res = await axios.get(endpoints.pokemonList(limit));
             if (res.data) {
                 const _pokemons = res.data.results.map((item: PokemonListResponse) => {
                     const separated = item.url.split("/");
-                    console.log(item.url);
                     return {
                         pokemonName: item.name,
                         // url contains the id of the Pokemon
-                        pokemonSprite: endpoints.sprite(separated[separated.length - 2])
+                        pokemonSprite: endpoints.sprite(separated[separated.length - 2]),
+                        colour: "linear-gradient(180deg, #4b91f7 0%, #367af6 100%)"
                     };
                 });
                 setPokemons(_pokemons);
