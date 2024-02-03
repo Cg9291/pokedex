7b28c68e38fcd4cc61e846b8e321d0a29988591c
CG <105956982+Cg9291@users.noreply.github.com>
2024-02-02 19:06:20 -0800

    added wrapper to pokemonguess page

--- src/pages/PokemonGuess.tsx
+++ src/pages/PokemonGuess.tsx
@@ -3,6 +3,8 @@ import { getPokemonGameList, PokemonGuessInfo } from "../functions/api/singleApi
 import styled from "styled-components/macro";
 import ContainerPrototype from "../components/prototypes/ContainerPrototype";
 import { capitalizeWords } from "../functions/utilities/capitalizeWords";
+import * as breakpoints from "../objects/breakpoints";
+
 export function PokemonGuess() {
     const [pokemonChoices, setPokemonChoices] = useState<PokemonGuessInfo[]>([]);
     const [correctPokemon, setCorrectPokemon] = useState<PokemonGuessInfo>();
@@ -58,38 +60,55 @@ export function PokemonGuess() {
 
     return (
         <Container>
-            <Title>Who is that Pokemon ?</Title>
-            <ImageContainer>
-                <HiddenImage $isOpen={isReveal} style={{ filter: `brightness(${isReveal ? 1 : 0})` }}>
-                    <PokemonImage href={correctPokemon?.pokemonSprite} />
-                </HiddenImage>
-            </ImageContainer>
-            <Choices>
-                {pokemonChoices &&
-                    pokemonChoices.map((item: PokemonGuessInfo, idx) => (
-                        <ChoiceButton
-                            key={idx}
-                            onClick={() => !isReveal && handleChoice(item.pokemonName)}
-                            style={{ background: item.colour }}
-                        >
-                            <h1>{capitalizeWords(item.pokemonName)}</h1>
-                        </ChoiceButton>
-                    ))}
-            </Choices>
+            <Wrapper>
+                <Title>Who is that Pokemon ?</Title>
+                <ImageContainer>
+                    <HiddenImage $isOpen={isReveal} style={{ filter: `brightness(${isReveal ? 1 : 0})` }}>
+                        <PokemonImage href={correctPokemon?.pokemonSprite} />
+                    </HiddenImage>
+                </ImageContainer>
+                <Choices>
+                    {pokemonChoices &&
+                        pokemonChoices.map((item: PokemonGuessInfo, idx) => (
+                            <ChoiceButton
+                                key={idx}
+                                onClick={() => !isReveal && handleChoice(item.pokemonName)}
+                                style={{ background: item.colour }}
+                            >
+                                <h1>{capitalizeWords(item.pokemonName)}</h1>
+                            </ChoiceButton>
+                        ))}
+                </Choices>
+            </Wrapper>
         </Container>
     );
 }
 
 const Container = styled(ContainerPrototype)`
+    @media ${breakpoints.widthsQueries.minWidths.laptop} {
+        padding: 0 12vw;
+        background-color: #1b252f;
+    }
+`;
+
+const Wrapper = styled(ContainerPrototype)`
     display: grid;
     grid-template-rows: 1fr 40vh 40vh;
     max-height: 100%;
     padding: 0 1rem;
+    @media ${breakpoints.widthsQueries.minWidths.laptop} {
+        background-color: white;
+    }
 `;
 
 const Title = styled.h1`
     //text-align: center;
     white-space: nowrap;
+    @media ${breakpoints.widthsQueries.minWidths.laptop} {
+        margin-top: 1rem;
+        text-align: center;
+        font-size: 4rem;
+    }
 `;
 
 const ChoiceButton = styled.button`
